72dc1e248b870ef93bab5b18a72ed9111cb55c46
Racchel <racchelvelasco@gmail.com>
2021-12-10 11:14:16 -0300

    adjustment config and jwt.utils

--- config/default.ts
+++ config/default.ts
@@ -2,6 +2,27 @@ export default {
     port: 1337,
     dbUri: "mongodb://teste:teste.21@localhost:27017/?authSource=admin",
     saltWorkFactor: 10, 
+    publicKey: ` -----BEGIN RSA PRIVATE KEY-----
+    MIICWwIBAAKBgHJCkQ7+ToltS9CZORqZK7jmQwit9CTZFReXc8ihphhu1vMH5dq9
+    mkqBFfECoxVqp5TYNnw/Lk8VpPC8iXizaDlhAH8KPa/m/g/e2Zsv0hffoqvyqTlT
+    IVlKWo3VScaPNyWxj+QyyPwgW3UJu7EWSAvR5wjMie6hJzImGqzslFMbAgMBAAEC
+    gYBJYascAAOci09Ht0BPbtP/uhKPPnkwOSMstZ4s2nR7+KOgDhB3PZIcC292DAWF
+    OaxLEdlcTo+FRxdZbRy1IjPq1BUVMWbyr1Xz/fW/l49WJlrpOBp8esmgpfZnRD+O
+    P+tA4IzbFcorYB/UTpYVTnNUfvcEVVKE/jsW2kRIpy4qQQJBANjY2weKSsD55pqk
+    xVUorucsCRdKkzXZrS1+cdzTjAOaIcaqygcyDNCI16TAG0qYsc+Mpr0L3zp8Undp
+    IGGdWxECQQCG4+nkYt2+psE5ii7/yCnW0odY1dHvW9HsFhbrGa3IMfOU+rVcIU9f
+    UzPBlok6AAhJceB1diOjV9IcX4TPPRNrAkEAs8JRD9ccUQzYlamxy/6Fvy1J+/ze
+    O+3PGb+QNDdKAlJrXX2HLJ/Y76YA3ZpDBXu/Kwa10lFdI+abllGXflQrgQJAckuu
+    RLnNIJ2VflbrKgqGASPUUV6kCwgnYL763mycxtQkjCi/AXisE56hbThLwJyEaQ8X
+    3Q+kdOSWATqigyNCTwJAYdJHBb6vikWRp4NY9K2JM0/OyWJKZ9iu4MiJlMQ7OnHu
+    XXQI3MQRvgbrSQF5iOa/Z5NXDtgduTy17fri2YuOaA==
+    -----END RSA PRIVATE KEY----- `,
+    privateKey: `-----BEGIN PUBLIC KEY-----
+    MIGeMA0GCSqGSIb3DQEBAQUAA4GMADCBiAKBgHJCkQ7+ToltS9CZORqZK7jmQwit
+    9CTZFReXc8ihphhu1vMH5dq9mkqBFfECoxVqp5TYNnw/Lk8VpPC8iXizaDlhAH8K
+    Pa/m/g/e2Zsv0hffoqvyqTlTIVlKWo3VScaPNyWxj+QyyPwgW3UJu7EWSAvR5wjM
+    ie6hJzImGqzslFMbAgMBAAE=
+    -----END PUBLIC KEY-----`
 };
 
 
